966a69475823f69d1273c93c725a07426016c082
CruzGusta <165952673+CruzGusta@users.noreply.github.com>
2025-06-03 03:45:35 +0000

    adicionado a lógica com if else, switch, operador ternario e as somas de 2 atributos escolhidos pelo jogador

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -11,8 +11,8 @@ int main() {
     char estadoA[3], estadoB[3];
     char codigoA [5], codigoB [5];
     char nomeCidA [30], nomeCidB [30];
-    int PturisticosA, PturisticosB, escolha;
-    float areaKmA, areaKmB, pibA, pibB, densiPopA, densiPopB, PIBca, PIBcb; 
+    int PturisticosA, PturisticosB, escolha, escolha2;
+    float areaKmA, areaKmB, pibA, pibB, densiPopA, densiPopB, PIBca, PIBcb, vA, vB, vA2, vB2, resultado, resultado2; 
     unsigned long int populacaoA, populacaoB; // uso de unsigned para grandes numeros
 
     // Cadastro das Cartas:
@@ -21,7 +21,7 @@ int main() {
     // utilize o código do primeiro desafio
       // perguntas para cadastro carta 1
     printf("--cadastro da carta A-- \n");
-    printf(" Estado : \n");
+    printf("Estado : \n");
     scanf(" %s", estadoA );
 
     printf("Código da carta : \n");
@@ -39,10 +39,10 @@ int main() {
     printf("PIB : \n");
     scanf("%f",&pibA );
 
-    printf("Pontos turisticos : \n");
+    printf("Pontos turísticos : \n");
     scanf("%d", &PturisticosA);
 
-    // calculo da densidade populacional impedindo possivel reotrno com 0
+    // calculo da densidade populacional impedindo possivel retorno com 0
     if (areaKmA != 0) {
     densiPopA = populacaoA / areaKmA;
 } else {
@@ -56,9 +56,8 @@ int main() {
     }
 
      // calculo do PIB per capita com casting
-   // densiPopREa = (float) areaKmA/populacaoA; // densidade populaconal reverso
     getchar();
-//carta 2
+          //carta 2
        // perguntas para cadastro
     printf("--cadastro da carta B-- \n");
     printf("Estado : \n"); 
@@ -79,14 +78,13 @@ int main() {
     printf("PIB : \n");
     scanf("%f",&pibB );
 
-
-    printf("Pontos turisticos : \n");
+    printf("Pontos turísticos : \n");
     scanf("%d", &PturisticosB);
 
-     // calculo da densidade populacional impedindo possivel reotrno com 0
+     // calculo da densidade populacional impedindo possivel retorno com 0
     if (areaKmB != 0) {
     densiPopB = populacaoB / areaKmB;
-} else {
+    } else {
     densiPopB = 0;
 }
   // if para proteger o calculo de divisão por 0 igual na densidade populacional
@@ -103,8 +101,8 @@ int main() {
     printf("**carta A** \n");
     printf("Estado : %s \n Código da carta : %s \n", estadoA, codigoA);
     printf("Nome da cidade : %s \n População : %lu \n",nomeCidA, populacaoA );
-    printf("Àrea : %.2f km² \n PIB : %.2f \n", areaKmA, pibA);
-    printf("Pontos turisticos : %d \n", PturisticosA);
+    printf("Área : %.2f km² \n PIB : %.2f \n", areaKmA, pibA);
+    printf("Pontos turísticos : %d \n", PturisticosA);
     printf("Densidade populacional : %.2f \n", densiPopA);
     printf("PIB per capita : %.2f \n", PIBca);
     printf("  \n");
@@ -113,8 +111,8 @@ int main() {
     printf("**carta B** \n");
     printf("Estado : %s \n Código da carta : %s \n", estadoB, codigoB);
     printf("Nome da cidade : %s \n População : %lu \n",nomeCidB, populacaoB );
-    printf("Àrea : %.2f km² \n PIB : %.2f \n", areaKmB, pibB);
-    printf("Pontos turisticos : %d \n", PturisticosB);
+    printf("Área : %.2f km² \n PIB : %.2f \n", areaKmB, pibB);
+    printf("Pontos turísticos : %d \n", PturisticosB);
     printf("Densidade populacional : %.2f \n", densiPopB);
     printf("PIB per capita : %.2f \n", PIBcb);
    printf("  \n");// pular linha por estetica
@@ -128,72 +126,111 @@ int main() {
     printf("Digite : 1 para População \n");
     printf("Digite : 2 para Área \n");
     printf("Digite : 3 para PIB \n");
-    printf("Digite : 4 para Numero de pontos turisticos \n");
+    printf("Digite : 4 para Numero de pontos turísticos \n");
     printf("Digite : 5 para Densidade populacional \n");
     printf("Digite : 6 para PIB per capita \n");
-    scanf("%d",&escolha);
+    scanf("%d", &escolha);
 
-    switch (escolha)
-    {
+    //switch de escolha 1
+    switch (escolha){
     case 1:
-      if(populacaoA > populacaoB){ //código da vitoria da carta A
-        printf("%s venceu por ter a população de %u , sendo maior que %s com %u \n ", nomeCidA, populacaoA, nomeCidB, populacaoB);
-      }else if(populacaoA < populacaoB){ // código da vitoria da carta B
-        printf("%s venceu por ter a população de %u , sendo maior que %s com %u \n ", nomeCidB, populacaoB, nomeCidA, populacaoA);
-      }else{
-        printf("***empatou, ambos tem o mesmo tanto***\n"); // caso de empate
-      }
+    vA = populacaoA ; vB = populacaoB;
+     printf("População. \n");
       break;
      case 2:
-      if(areaKmA > areaKmB){
-        printf("%s venceu por ter a área de %.2f km², sendo maior que %s com %.2f km² \n ", nomeCidA, areaKmA, nomeCidB, areaKmB);
-      }else if (areaKmA < areaKmB){
-        printf("%s venceu por ter a área de %.2f km², sendo maior que %s com %.2f km² \n ", nomeCidB, areaKmB, nomeCidA, areaKmA);
-      }else{
-         printf("***empatou, ambos tem o mesmo tanto***\n");
-      }
+     vA = areaKmA ; vB = areaKmB;
+      printf("Área. \n");
       break;
      case 3:
-      if( pibA > pibB){
-        printf(" %s venceu por ter o PIB de %.2f, maior que %s com %.2f \n",nomeCidA, pibA, nomeCidB, pibB );
-      }else if (pibA < pibB){ 
-        printf(" %s venceu por ter o PIB de %.2f, maior que %s com %.2f \n",nomeCidB, pibB, nomeCidA, pibA );
-      }else{
-        printf("***empatou, ambos tem o mesmo tanto***\n");
-      }
+     vA = pibA ; vB = pibB;
+     printf("PIB. \n");
       break;
      case 4:
-      if( PturisticosA > PturisticosB){
-        printf("%s venceu por ter %d pontos turisticos, sendo mais que %s com %d \n ", nomeCidA, PturisticosA, nomeCidB, PturisticosB);
-      }else if(PturisticosA < PturisticosB){
-        printf("%s venceu por ter %d pontos turisticos, sendo mais que %s com %d \n ", nomeCidB, PturisticosB, nomeCidA, PturisticosA);
-      }else{
-        printf("***empatou, ambos tem o mesmo tanto***\n");
-      }
+     vA = PturisticosA ; vB = PturisticosB;
+     printf("Pontos turísticos. \n");
       break;
      case 5:
-      if(densiPopA < densiPopB){
-        printf("%s venceu por ter a densidade populacional de %.2f , sendo menor que %s com %.2f \n ", nomeCidA, densiPopA, nomeCidB, densiPopB);
-      }else if(densiPopA > densiPopB){
-        printf("%s venceu por ter a densidade populacional de %.2f , sendo menor que %s com %.2f \n ", nomeCidB, densiPopB, nomeCidA, densiPopA);
-      }else{
-        printf("***empatou, ambos tem o mesmo tanto***\n");
-      }
+     vA = densiPopA ; vB = densiPopB;
+     printf(" Densidade populacional. \n");
       break;
      case 6:
-      if(PIBca > PIBcb){
-        printf("%s venceu por ter o PIB per capita de %.2f , sendo maior que %s com %.2f  \n ", nomeCidA, PIBca, nomeCidB, PIBcb);
-      }else if(PIBca < PIBcb){
-        printf("%s venceu por ter o PIB per capita de %.2f , sendo maior que %s com %.2f \n ", nomeCidB, PIBcb, nomeCidA, PIBca);
-      }else{
-        printf("***empatou, ambos tem o mesmo tanto***\n");
-      }
+     vA = PIBca; vB = PIBcb;
+     printf(" PIB per capita. \n");
       break;
     default:
-    printf(" Numero invalido.");
+    printf(" Numero invalido. \n");
       break;
     }
+  printf(" escolha uma segunda opção de atributo: \n");
+  printf(" Por favor, escolha um atributo diferente do anterior. \n");
+  printf("Digite : 1 para População \n");
+  printf("Digite : 2 para Área \n");
+  printf("Digite : 3 para PIB \n");
+  printf("Digite : 4 para Numero de pontos turísticos \n");
+  printf("Digite : 5 para Densidade populacional \n");
+  printf("Digite : 6 para PIB per capita \n");
+  scanf("%d", &escolha2);
 
+ if(escolha == escolha2){   // if para impedir que o mesmo atributo seja repetido
+   printf("não pode escolher o mesmo atributo duas vezes \n");
+ }else{
+   // switch da segunda escolha de atributo em caso de não tr o mesmo atributo selecionado
+  switch (escolha2){ 
+  case 1 :
+    vA2 = populacaoA ; vB2 = populacaoB;
+    printf("População. \n ");
+    break;
+   case 2:
+    vA2 = areaKmA ; vB2 = areaKmB;
+    printf("Área. \n");
+    break;
+   case 3:
+    vA2 = pibA ; vB2 = pibB;
+    printf("PIB. \n");
+    break;
+   case 4 :
+    vA2 = PturisticosA ; vB2 = PturisticosB;
+    printf("Pontos turísticos. \n");
+    break;
+   case 5 :
+    vA2 = densiPopA ; vB2 = densiPopB;
+    printf(" Densidade populacional. \n");
+    break;
+   case 6 :
+    vA2 = PIBca; vB2 = PIBcb;
+    printf(" PIB per capita. \n");
+    break;
+
+  default:
+  printf("Numero invalido.\n");
+    break;
+  }
+
+ }
+// em caso de escolher a opção 5, deixar o valor menor como vencedor
+ vA = (escolha == 5) ? -vA : vA;
+ vB = (escolha == 5) ? -vB : vB;
+ vA2 = (escolha2 == 5) ? -vA2 : vA2;
+ vB2 = (escolha2 == 5) ? -vB2 : vB2;
+
+ // soma dos resultados das duas escolhas
+ resultado = vA + vA2;
+ resultado2 = vB + vB2;
+
+ //print da soma dos resultados
+ printf("A soma dos resultados dos atributos de %s é %.2f \n",nomeCidA, resultado );
+ printf("A soma dos resultados dos atributos de %s é %.2f \n",nomeCidB, resultado2);
+
+ // condição que determina o vencedor ou se houve um empate
+ if(resultado > resultado2){
+  printf("%s ganhou de %s pela soma dos resultados de %.2f a %.2f \n", nomeCidA, nomeCidB, resultado, resultado2);
+ }else if(resultado == resultado2){
+  printf("Houve um empate entre %s e %s pois contam com a soma de %.2f e %.2f \n", nomeCidA, nomeCidB, resultado, resultado2);
+ }else{
+  printf("%s ganhou de %s pela soma dos resultados de %.2f a %.2f \n", nomeCidB, nomeCidA, resultado2, resultado);
+ }
+
+ 
 
 
     return 0;
